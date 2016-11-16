65c1520d9c63e14ff297c91268b292bb16418082
lancelot-55 <blaurosen55@gmail.com>
2016-11-16 14:07:33 +0900

    グレイスケール化,二値化メソッド実装

--- Thinning.java
+++ Thinning.java
@@ -1,55 +1,97 @@
 /* 
    --画像処理演習課題--
-   所属:
-   学籍番号:
-   氏名:
+   所属: 情報学群2年
+   学籍番号: 1190304
+   氏名: 氏原友梨亜
 */
 import java.io.*;
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 
 class Thinning {
+    public static void main(String args[]){
+	try{
+            /* 入力画像の読み込み */
+	    BufferedImage readImage = ImageIO.read(new File("sample.png"));
+	    int w = readImage.getWidth(); //横幅
+	    int h = readImage.getHeight(); //縦幅
 
-	public static void main(String args[]){
-		try{
+	    /* 出力画像の準備 */
+	    BufferedImage writeImage = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
 
-			/* 入力画像の読み込み */
-			BufferedImage readImage = ImageIO.read(new File(args[0])); //第一引数をファイル名とする
-			int w = readImage.getWidth(); //横幅
-			int h = readImage.getHeight(); //縦幅
+	    /* コピーを行う処理 */
+	    copy(readImage, writeImage, w,h);
 
-			/* 出力画像の準備 */
-			BufferedImage writeImage = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
+	    /* グレイスケール化を行う処理 */
+	    grayScale(writeImage, w, h);
 
-		    /*
-		     * 細線化処理を記述してください。
-		     * 必要に応じて変数・メソッドの定義を行ってかまいません。
-		     * 例として下記にコピーを行うだけのメソッドを記述します。
-		     */
-		    /* 例:コピーを行う処理 */
-		    copy(readImage, writeImage, w,h);
+	    /* 二値化を行う処理 */
+	    binarization(writeImage, w, h);
 
-		    /* output.pngへの書き込み */
-		    ImageIO.write(writeImage, "png", new File("output.png"));
+	    /* 細線化を行う処理 */
+	    
+	    /* output.pngへの書き込み */
+	    ImageIO.write(writeImage, "png", new File("output.png"));
 
-		}catch (IOException e){
-			/* 例外処理 */
-			throw new RuntimeException(e.toString());
-		}
-		System.out.println("画像処理が完了しました");
+	}catch (IOException e){
+	    /* 例外処理 */
+	    throw new RuntimeException(e.toString());
 	}
+        System.out.println("画像処理が完了しました");
+    }
 
-	/*
-	 * メソッドの定義
-	 */
-	/* コピーメソッド */
-	public static void copy(BufferedImage readImage, BufferedImage writeImage, int w, int h){
-		// 1ピクセルづつ処理を行う
-		for (int y = 0; y < h; y++) {
-		    for (int x = 0; x < w; x++) {
-				int color = readImage.getRGB(x, y); // 入力画像の画素値を取得
-				writeImage.setRGB(x, y, color); //出力画像に画素値をセット			
-		    }
-		}
+    /* コピーメソッド */
+    public static void copy(BufferedImage readImage, BufferedImage writeImage, int w, int h){
+	// 1ピクセルづつ処理を行う
+	for (int y = 0; y < h; y++) {
+	    for (int x = 0; x < w; x++) {
+		int color = readImage.getRGB(x, y); // 入力画像の画素値を取得
+		writeImage.setRGB(x, y, color); //出力画像に画素値をセット			
+	    }
 	}
+    }
+    /* グレイスケール化メソッド */
+    public static void  grayScale(BufferedImage readImage, int w, int h) {
+	// 出力画像をバイトグレースケールで表示
+	BufferedImage output = new BufferedImage(w, h, BufferedImage.TYPE_BYTE_GRAY);
+    	output.setRGB(0, 0, w, h, readImage.getRGB(0, 0, w, h, null, 0, w), 0, w);
+        for (int y = 0; y < h; y++) {
+	    for (int x = 0; x < w; x++) {
+		int color = output.getRGB(x, y); // 入力画像の画素値を取得
+		readImage.setRGB(x, y, color); //出力画像に画素値をセット			
+	    }
+	}
+    }
+    /* 二値化メソッド */
+    public static void binarization(BufferedImage readImage, int w, int h) {
+	BufferedImage output = new BufferedImage(w, h, BufferedImage.TYPE_BYTE_GRAY);
+	for (int y = 0; y < h; y++) {
+	    for (int x = 0; x < w; x++) {
+		int color = readImage.getRGB(x, y);
+		int mono = (int)(0.299 * r(color) + 0.587 * g(color) + 0.114 * b(color));
+		if(mono > 125) {
+		    output.setRGB(x, y, 0xFFFFFF);
+		} else {
+		    output.setRGB(x, y, 0x000000);
+		}
+	    }
+	}
+	for (int y = 0; y < h; y++) {
+	    for (int x = 0; x < w; x++) {
+		int color = output.getRGB(x, y); // 入力画像の画素値を取得
+		readImage.setRGB(x, y, color); //出力画像に画素値をセット			
+	    }
+	}
+    }
+    public static int r(int c){
+	    return c>>16&0xff;
+    }
+    public static int g(int c){
+	    return c>>8&0xff;
+    }
+    public static int b(int c){
+	    return c&0xff;
+     }
+    
+    /* 細線化メソッド */
 }
