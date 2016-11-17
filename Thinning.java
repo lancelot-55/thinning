add3d55a0f1f9d683da065eef32005ae0fa7541d
lancelot-55 <blaurosen55@gmail.com>
2016-11-17 13:26:37 +0900

    修正

--- Thinning.java
+++ Thinning.java
@@ -21,7 +21,7 @@ class Thinning {
 	    /* グレイスケール化を行う処理 */
 	    grayScale(writeImage, w, h);
 	    /* 二値化を行う処理 */
-	    byte[][] binary = new byte[w][h];
+	    byte[][] binary = new byte[w][h]; // 画素が黒の場合0，白の場合1の値をとる
 	    binarization(writeImage, binary, w, h);
 	    /* 細線化を行う処理 */
 	    thinning(writeImage, binary, w, h);	    
@@ -83,16 +83,11 @@ class Thinning {
      } 
     /* 田村のアルゴリズムによる細線化メソッド */
     public static void thinning(BufferedImage readImage, byte[][] binary, int w, int h) {
-        byte[][] delete_pix = new byte[w][h];
+        byte[][] delete_pix = new byte[w][h]; // 色を黒に変更する画素の情報を保持
 	while(true) {
-	    //int count = 0;
-	    //thin(binary, delete_pix, count, w, h, 1);
-	    //if(count == 0) break;
-	    //count = 0;
-	    //thin(binary, delete_pix, count, w, h, 2);
-	    //if(count == 0) break;
-	    int count = 0;
-	    for(int y = 0; y < h-2; y++) {
+	    /* パターン1の除去(画素の色を白から黒へ変更)パターンを調べる */
+	    int count = 0; // 色を変更する画素の数
+	    for(int y = 0; y < h-2; y++) { // 全画素に対して行う
 		for(int x = 0; x < w-2; x++) {
 		    byte[] p = new byte[9];
 		    p[0] = binary[x][y];
@@ -104,21 +99,22 @@ class Thinning {
 		    p[6] = binary[x][y+2];
 		    p[7] = binary[x+1][y+2];
 		    p[8] = binary[x+2][y+2];
-		    if(!delete(p, 1)) continue;
-		    if(nodelete(p, 1)) continue;
-		    if(con_nodelete(p)) continue;
-		    delete_pix[x+1][y+1] = 1;
+		    if(!delete(p, 1)) continue; // パターン1の除去パターンにマッチしなければ次の画素を調べる
+		    if(nodelete(p, 1)) continue; // パターン1の非除去パターンにマッチすれば次の画素を調べる
+		    if(con_nodelete(p)) continue; // 両パターン共通の非除去パターンにマッチすれば次の画素を調べる
+		    delete_pix[x+1][y+1] = 1; // 3*3の中央の画素の除去を決定する
 		    count++;
 		}
 	    }
-	    for(int y = 0; y < h; y++) {
+	    for(int y = 0; y < h; y++) { // binaryの更新
 		for(int x = 0; x < w; x++) {
 		    if(delete_pix[x][y] == 1) binary[x][y] = 0;
 		}
 	    }
-	    if(count == 0) break;
+	    if(count == 0) break; // 除去する画素がなければ終了
+	    /* パターン2の除去パターンを調べる */
 	    count = 0;
-	    for(int y = 0; y < h-2; y++) {
+	    for(int y = 0; y < h-2; y++) { // 全画素に対して行う
 		for(int x = 0; x < w-2; x++) {
 		    byte[] p = new byte[9];
 		    p[0] = binary[x][y];
@@ -130,21 +126,21 @@ class Thinning {
 		    p[6] = binary[x][y+2];
 		    p[7] = binary[x+1][y+2];
 		    p[8] = binary[x+2][y+2];
-		    if(!delete(p, 2)) continue;
-		    if(nodelete(p, 2)) continue;
-		    if(con_nodelete(p)) continue;
-		    delete_pix[x+1][y+1] = 1;
+		    if(!delete(p, 2)) continue; // パターン2の除去パターンにマッチしなければ次の画素を調べる
+		    if(nodelete(p, 2)) continue; // パターン2の非除去パターンにマッチすれば次の画素を調べる
+		    if(con_nodelete(p)) continue; // 両パターン共通の非除去パターンにマッチすれば次の画素を調べる
+		    delete_pix[x+1][y+1] = 1; // 3*3の中央の画素の除去を決定する
 		    count++;
 		}
 	    }
-	    for(int y = 0; y < h; y++) {
+	    for(int y = 0; y < h; y++) { // binaryの更新
 		for(int x = 0; x < w; x++) {
 		    if(delete_pix[x][y] == 1) binary[x][y] = 0;
 		}
 	    }
-	    if(count == 0) break;
-	}
-	for(int y = 0; y < h; y++) {
+	    if(count == 0) break; // 除去する画素がなければ終了
+	}	
+	for(int y = 0; y < h; y++) { // 元画像の更新
 	    for(int x = 0; x < w; x++) {
 		if(binary[x][y] == 1) {
 		    readImage.setRGB(x, y, 0xffffff);
@@ -154,50 +150,27 @@ class Thinning {
 	    }
 	}
     }
-    public static void thin(byte[][] binary, byte[][] delete_pix, int count, int w, int h, int petern) {
-	for(int y = 0; y < h-2; y++) {
-	    for(int x = 0; x < w-2; x++) {
-		byte[] p = new byte[9];
-		p[0] = binary[x][y];
-		p[1] = binary[x+1][y];
-		p[2] = binary[x+2][y];
-		p[3] = binary[x][y+1];
-		p[4] = binary[x+1][y+1];
-		p[5] = binary[x+2][y+1];
-		p[6] = binary[x][y+2];
-		p[7] = binary[x+1][y+2];
-		p[8] = binary[x+2][y+2];
-		if(!delete(p, petern)) continue;
-		if(nodelete(p, petern)) continue;
-		if(con_nodelete(p)) continue;
-		delete_pix[x+1][y+1] = 1;
-		count++;
-	    }
-	}
-	for(int y = 0; y < h; y++) {
-	    for(int x = 0; x < w; x++) {
-		if(delete_pix[x][y] == 1) binary[x][y] = 0;
-	    }
-	}
-    }
+    /* 3*3の除去(白から黒へ変更)パターン */
     public static boolean delete(byte[] p, int petern) {
-	if(petern == 1) {
+	if(petern == 1) { // パターン1
 	    if((p[1]==0 && p[4]==1) || (p[4]==1 && p[5]==0)) return true;
-	} else {
+	} else { // パターン2
 	    if((p[4]==1 && p[7]==0) || (p[3]==0 && p[4]==1)) return true;
 	}
 	return false;
     }
+    /* 3*3の非除去パターン */
     public static boolean nodelete(byte[] p, int petern) {
-	if(petern == 1) {
+	if(petern == 1) { // パターン1
 	    if((p[1]==0 && p[4]==1 && p[5]==1 && p[7]==1 && p[8]==0) ||
 	       (p[0]==0 && p[1]==1 && p[3]==1 && p[4]==1 && p[5]==0)) return true;
-	} else {
+	} else { // パターン2
 	    if((p[0]==0 && p[1]==1 && p[3]==1 && p[4]==1 && p[7]==0) ||
 	       (p[3]==0 && p[4]==1 && p[5]==1 && p[7]==1 && p[8]==0)) return true;
 	}
 	return false;
     }
+    /* パターン1，2共通の3*3の非除去パターン */
     public static boolean con_nodelete(byte[] p) {
 	if((p[3]==0 && p[4]==1 && p[5]==0 && p[7]==1) ||
 	   (p[1]==0 && p[3]==1 && p[4]==1 && p[7]==0) ||
